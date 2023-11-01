139f51c97cc481d8c0b7b3758612e56b8b232182
sagaramundla <143153022+sagaramundla@users.noreply.github.com>
2023-11-01 15:38:59 -0500

    books pug

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var BookRouter = require('./routes/Book');
+var bookRouter = require('./routes/book');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Book', BookRouter);
+app.use('/book', bookRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
